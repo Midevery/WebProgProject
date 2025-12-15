f949123d390104b6064f23604ff4a478b3e23313
ELROY HANS ANGGADA <elroyhans@gmail.com>
2025-12-15 16:03:31 +0700

    Force HTTPS for Render deployment

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,24 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        
+        // Paksa HTTPS di Production (Render)
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
