0e3659abb8d59fd13675a9b5124c1fbdd2a1ead6
ELROY HANS ANGGADA <elroyhans@gmail.com>
2025-12-15 14:47:37 +0700

    Fix read-only file system vercel

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+
+$app->handleRequest(Request::capture());
