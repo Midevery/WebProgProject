52ff0736dc8ee40b4c4ab7e2387e5e841abdc907
ELROY HANS ANGGADA <elroyhans@gmail.com>
2025-12-15 14:50:09 +0700

    Fix read-only file system vercel

--- backend/api/index.php
+++ backend/api/index.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+$tmpPath = '/tmp';
+
+$_ENV['APP_PACKAGES_CACHE'] = $tmpPath . '/packages.php';
+$_ENV['APP_SERVICES_CACHE'] = $tmpPath . '/services.php';
+$_ENV['APP_ROUTES_CACHE'] = $tmpPath . '/routes-v7.php';
+$_ENV['APP_EVENTS_CACHE'] = $tmpPath . '/events.php';
+$_ENV['APP_CONFIG_CACHE'] = $tmpPath . '/config.php';
+
+putenv('APP_PACKAGES_CACHE=' . $_ENV['APP_PACKAGES_CACHE']);
+putenv('APP_SERVICES_CACHE=' . $_ENV['APP_SERVICES_CACHE']);
+putenv('APP_ROUTES_CACHE=' . $_ENV['APP_ROUTES_CACHE']);
+putenv('APP_EVENTS_CACHE=' . $_ENV['APP_EVENTS_CACHE']);
+putenv('APP_CONFIG_CACHE=' . $_ENV['APP_CONFIG_CACHE']);
 
 require __DIR__ . '/../vendor/autoload.php';
 
