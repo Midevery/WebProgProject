acbfb0ba6c636d4d91452291707722cdfbac0de0
ELROY HANS ANGGADA <elroyhans@gmail.com>
2025-12-15 14:06:37 +0700

    fix seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            TechMarketSeeder::class,
+            CompleteDataSeeder::class,
         ]);
     }
 }
